f5979a2e9bd5dde6b3face28414bb21dd70a7729
yaseminkilic <ysmnkilic93@gmail.com>
2016-10-30 19:12:20 +0200

    Changed accessToken/Secret and customerKey/Secret Signed-off-by: yaseminkilic <ysmnkilic93@gmail.com>

---
+++ TwitterAPP/src/Oauth.java
@@ -0,0 +1,29 @@
+
+class Oauth {
+
+	private static String consumerKey;
+	private static String consumerSecret;
+	
+	Oauth(String key, String secret){
+		consumerKey = key;
+		consumerSecret = secret;
+	}
+	
+	
+	public static String getConsumerKey() {
+		return consumerKey;
+	}
+	
+	public static void setConsumerKey(String consumerKey) {
+		Oauth.consumerKey = consumerKey;
+	}
+	
+	public static String getConsumerSecret() {
+		return consumerSecret;
+	}
+	
+	public static void setConsumerSecret(String consumerSecret) {
+		Oauth.consumerSecret = consumerSecret;
+	}
+	
+}
